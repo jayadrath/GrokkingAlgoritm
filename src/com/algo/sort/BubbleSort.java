12f9312f324cca02dff661c7ed27699ac572b0d2
Kaushik_Chakraborty <kaushik.chakraborty@mheducation.com>
2022-04-17 16:11:13 +0530

    Fixed minor issue with bubble sort algorithm

--- src/com/algo/sort/BubbleSort.java
+++ src/com/algo/sort/BubbleSort.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class BubbleSort {
 	public static void main(String[] args) {
-		int[] arr = {15,2 ,6,4,34,9, 102, 17};
+		int[] arr = {15,2 ,6,4,34,9, 102, 17, 1, 100};
 		bubbleSort(arr);
 		Arrays.stream(arr).forEach(System.out::println);
  	}
 
 	private static void bubbleSort(int[] arr) {
 		int temp;
+		int decLen = arr.length;
 		for(int i = 0; i < arr.length; i++) {
-			int decLen = arr.length;
 			for(int j = 0; j< decLen-1; j++) {
 				if(arr[j] > arr[j+1]) {
 					temp = arr[j];
